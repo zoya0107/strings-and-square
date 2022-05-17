e0c41573ff63b325591188f70b9c9583d3a856c0
Zoya Nazarova <zoyavishni@gmail.com>
2022-05-17 13:42:12 +0700

    initial commit

---
+++ src/main/java/com/pet/sas/stringsandsquare/StringsAndSquareApplication.java
@@ -0,0 +1,13 @@
+package com.pet.sas.stringsandsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringsAndSquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringsAndSquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet/sas/stringsandsquare/StringsAndSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.sas.stringsandsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsAndSquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
