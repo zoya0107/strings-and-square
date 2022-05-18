b2e0ce9ef35c57fb7656f8c9861e6f686f5c7ec0
Zoya Nazarova <zoyavishni@gmail.com>
2022-05-19 03:15:04 +0700

    file_processing: added saving to json file (export)

--- src/main/java/com/pet/sas/stringsandsquare/controller/SquareController.java
+++ src/main/java/com/pet/sas/stringsandsquare/controller/SquareController.java
@@ -1,5 +1,7 @@
 package com.pet.sas.stringsandsquare.controller;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pet.sas.stringsandsquare.calculation.SquareTask;
 import com.pet.sas.stringsandsquare.model.SquareModel;
 import com.pet.sas.stringsandsquare.model.TaskType;
@@ -13,7 +15,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/home/square")
@@ -30,7 +37,7 @@ public class SquareController {
         this.squareTask = squareTask;
     }
 
-    @PostMapping(params = "savesquare")
+    @PostMapping(params = "save")
     public String saveNewSquareTask(@ModelAttribute("square") SquareModel squareModel,
                                     @ModelAttribute("type") TypeModel typeModel, Model model) {
         typeModel.setType(TaskType.SQUARE);
@@ -41,9 +48,28 @@ public class SquareController {
         return "redirect:/home/square";
     }
 
-    @PostMapping(params = "squaretask")
+    @PostMapping(params = "calculate")
     public String calculateCost(@ModelAttribute("square") SquareModel squareModel, Model model) {
         model.addAttribute("result", squareTask.costOfMagicSquare(squareModel));
+        return "square-page";
+    }
+
+    @PostMapping(params = "export")
+    public String exportTheSquare(@ModelAttribute("square") SquareModel squareModel,
+                                  @ModelAttribute("type") TypeModel typeModel) {
+        typeModel.setType(TaskType.SQUARE);
+        File file = new File("target/square.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> taskMap = new HashMap<>();
+        taskMap.put("numbers", squareModel);
+        taskMap.put("task", typeModel);
+        try {
+            JsonGenerator g = objectMapper.getFactory().createGenerator(new FileOutputStream(file));
+            objectMapper.writeValue(g, taskMap);
+            g.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "square-page";
     }
 }
--- src/main/java/com/pet/sas/stringsandsquare/controller/StringsController.java
+++ src/main/java/com/pet/sas/stringsandsquare/controller/StringsController.java
@@ -1,5 +1,7 @@
 package com.pet.sas.stringsandsquare.controller;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pet.sas.stringsandsquare.calculation.StringsTask;
 import com.pet.sas.stringsandsquare.model.StringsModel;
 import com.pet.sas.stringsandsquare.model.TaskType;
@@ -12,7 +14,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/home/strings")
@@ -28,7 +35,7 @@ public class StringsController {
         this.stringsTask = stringsTask;
     }
 
-    @PostMapping(params = "savestrings")
+    @PostMapping(params = "save")
     public String saveNewStringsTask(@ModelAttribute("strings") StringsModel stringsModel,
                                      @ModelAttribute("type") TypeModel typeModel, Model model) {
         typeModel.setType(TaskType.STRINGS);
@@ -39,9 +46,28 @@ public class StringsController {
         return "redirect:/home/strings";
     }
 
-    @PostMapping(params = "stringstask")
+    @PostMapping(params = "calculate")
     public String filterTheStrings(@ModelAttribute("strings") StringsModel stringsModel, Model model) {
         model.addAttribute("result", stringsTask.substrings(stringsModel));
+        return "strings-page";
+    }
+
+    @PostMapping(params = "export")
+    public String exportTheStrings(@ModelAttribute("strings") StringsModel stringsModel,
+                                   @ModelAttribute("type") TypeModel typeModel) {
+        typeModel.setType(TaskType.STRINGS);
+        File file = new File("target/strings.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> taskMap = new HashMap<>();
+        taskMap.put("strings", stringsModel);
+        taskMap.put("task", typeModel);
+        try {
+            JsonGenerator g = objectMapper.getFactory().createGenerator(new FileOutputStream(file));
+            objectMapper.writeValue(g, taskMap);
+            g.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "strings-page";
     }
 }
