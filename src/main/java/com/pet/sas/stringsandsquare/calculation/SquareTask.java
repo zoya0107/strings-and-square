efdee5d3dd9ba2ef5eecd8bedb84516d41178664
Zoya Nazarova <zoyavishni@gmail.com>
2022-05-21 01:35:39 +0700

    fix: fixed semi-magic calculations and view

--- src/main/java/com/pet/sas/stringsandsquare/calculation/SquareTask.java
+++ src/main/java/com/pet/sas/stringsandsquare/calculation/SquareTask.java
@@ -28,23 +28,18 @@ public class SquareTask {
     public int costOfSemiMagicSquare(SquareModel squareModel) {
         List<Integer> per11 = Arrays.asList(7, 6, 2, 9, 4, 8, 3, 5);
         List<Integer> per12 = Arrays.asList(2, 6, 7, 5, 3, 8, 4, 9);
-        List<Integer> per21 = Arrays.asList(1, 9, 5, 7, 3, 4, 8, 6);
-        List<Integer> per22 = Arrays.asList(5, 9, 1, 6, 8, 4, 3, 7);
+
         List<Integer> per31 = Arrays.asList(9, 5, 1, 8, 6, 7, 2, 4);
         List<Integer> per32 = Arrays.asList(1, 5, 9, 4, 2, 7, 6, 8);
-        List<Integer> per41 = Arrays.asList(6, 2, 7, 3, 5, 9, 1, 8);
-        List<Integer> per42 = Arrays.asList(7, 2, 6, 8, 1, 9, 5, 3);
-        List<Integer> per61 = Arrays.asList(5, 1, 9, 2, 4, 8, 3, 7);
-        List<Integer> per62 = Arrays.asList(9, 1, 5, 7, 3, 8, 4, 2);
+
+
         List<Integer> per71 = Arrays.asList(1, 5, 9, 2, 4, 3, 8, 6);
         List<Integer> per72 = Arrays.asList(9, 5, 1, 6, 8, 3, 4, 2);
-        List<Integer> per81 = Arrays.asList(5, 1, 9, 4, 2, 6, 7, 3);
-        List<Integer> per82 = Arrays.asList(9, 1, 5, 3, 7, 6, 2, 4);
+
         List<Integer> per91 = Arrays.asList(7, 2, 6, 1, 8, 4, 3, 5);
         List<Integer> per92 = Arrays.asList(6, 2, 7, 5, 3, 4, 8, 1);
-        List<List<Integer>> list = Arrays.asList(per21, per22, per31, per32, per41, per42, per61,
-                                                 per62, per71, per72, per81, per82, per91, per92);
-        List<Integer> listcp = Arrays.asList(2, 3, 4, 6, 7, 8, 9);
+        List<List<Integer>> list = Arrays.asList(per31, per32, per71, per72, per91, per92);
+        List<Integer> listcp = Arrays.asList(3, 7, 9);
         List<Integer> check = new ArrayList<>();
         check.add(squareModel.getA1());
         check.add(squareModel.getA2());
@@ -56,9 +51,12 @@ public class SquareTask {
         check.add(squareModel.getA4());
         int cc = squareModel.getA5();
         int min = countCost(cc, 1, check, per11, per12);
-        for (int i = 0; i < 7; i++) {
-            if (countCost(cc, listcp.get(i), check, list.get(i*2), list.get(i*2 + 1)) < min) {
-                min = countCost(cc, i, check, list.get(i), list.get(i + 1));
+        int temp;
+        for (int i = 0; i < 3; i++) {
+            temp = countCost(cc, listcp.get(i), check, list.get(i*2), list.get(i*2 + 1));
+            if (temp < min) {
+                System.out.println(i);
+                min = temp;
             }
         }
         return min;
